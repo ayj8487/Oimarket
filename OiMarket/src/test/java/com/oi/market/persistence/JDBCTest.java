2efe7e1299ba8df650d865242704e17daabf900a
Admin <Admin@DESKTOP-7M5IVBI>
2021-06-10 17:22:28 +0900

    spring 초기설정+mySql연동

--- OiMarket/src/test/java/com/oi/market/persistence/JDBCTest.java
+++ OiMarket/src/test/java/com/oi/market/persistence/JDBCTest.java
@@ -21,7 +21,7 @@ public class JDBCTest {
         try(Connection con = DriverManager.getConnection( 
                 "jdbc:mysql://localhost:3306/test?serverTimezone=Asia/Seoul", 
                 "root", 
-                "tiger")){ 
+                "1234")){ 
             System.out.println(con); 
         } catch (Exception e) { 
             fail(e.getMessage()); 
